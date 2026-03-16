845de50240c3b98fd58829f11fceb915ef0970d1
DaphnéAutrive <daphne.dematosp@icloud.com>
2026-03-16 11:55:44 +0100

    feat: add get reservations route

--- server/src/routes/reservations.ts
+++ server/src/routes/reservations.ts
@@ -1,1 +1,14 @@
 // Route definitions for reservations
+
+// Import Router from Express to create a router
+import { Router } from 'express'
+// Import function from the controller
+import { getReservations } from '../controllers/reservations.js'
+
+// Create an router Express
+const router = Router()
+
+// When someone calls GET /api/reservations execute getReservations
+router.get('/', getReservations)
+
+export default router
