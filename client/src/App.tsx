2d0b0f68f2fa3c483d69407a3a99c74c121e044c
DuteilValentin <valentin.duteil@oclock.school>
2026-03-17 14:51:09 +0100

    feat: register routes added to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 // Entry point for application routes
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// Import the register page
+import Register from './pages/Register'
 // Import the login page
 import Login from './pages/Login'
 // Import the reservation page
@@ -11,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reservation" element={<Reservation />} />
         <Route path="/my-account/reservations" element={<MyReservations />} />
