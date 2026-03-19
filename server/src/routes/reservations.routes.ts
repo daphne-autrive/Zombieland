cf7379382f6db065cabde1fa60f91648d6fd39da
DaphnéAutrive <daphne.dematosp@icloud.com>
2026-03-19 10:23:19 +0100

    feat: add role-based acces control on reservations routes

--- server/src/routes/reservations.routes.ts
+++ server/src/routes/reservations.routes.ts
@@ -5,14 +5,15 @@ import { Router } from 'express'
 // Import function from the controller
 import { createReservation, deleteReservation, getAllReservations, getMyReservations } from '../controllers/reservations.controller.js'
 import { checkToken } from '../middlewares/auth.middleware.js'
+import { checkRole } from '../middlewares/role.middleware.js'
 
 // Create an router Express
 const router = Router()
 
-// when someone calls GET /api/reservations/me, execute getMyReservations
+// when someone calls GET /api/reservations/me, execute getMyReservations (before /:id)
 router.get('/me', checkToken, getMyReservations)
-// When someone calls GET /api/reservations execute getAllReservations
-router.get('/', checkToken, getAllReservations)
+// When someone calls GET /api/reservations execute getAllReservations (admin only)
+router.get('/', checkToken, checkRole("ADMIN"), getAllReservations)
 // When someone calls POST /api/reservations, execute createReservation
 router.post('/', checkToken, createReservation)
 //When someone calls DELETE /api/reservations/:id, execute deleteReservation
