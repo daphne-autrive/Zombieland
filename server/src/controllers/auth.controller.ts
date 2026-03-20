477c99e24529c8ba454c61c980a29af8dfeeec39
yohann422 <fufulelapinou@gmail.com>
2026-03-20 09:54:05 +0100

    fix: secure update and samesite

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -64,7 +64,7 @@ export async function register(req: Request, res: Response, next: NextFunction) 
   res.cookie('token', token, {
     httpOnly: true,                 // JavaScript can't read it → protection XSS
     secure: true,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
-    sameSite: 'lax',                // cookie is send only from the same website → CSRF protection
+    sameSite: 'none',                // cookie is send only from the same website → CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000 // durée de vie en millisecondes
   })
 
@@ -119,7 +119,7 @@ export async function login(req: Request, res: Response, next: NextFunction) {
   res.cookie('token', token, {
     httpOnly: true,                 // JavaScript can't read it → protection XSS
     secure: true,                  // false for dev (HTTP, localhost), true for prod (HTTPS uniquement)
-    sameSite: 'lax',                // cookie is send only from the same website → CSRF protection
+    sameSite: 'none',                // cookie is send only from the same website → CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000 // durée de vie en millisecondes
   })
 
